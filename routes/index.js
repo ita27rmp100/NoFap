a55daa749e398d8ea3fb0bcad605cc028ab336d9
Abderahim <ita27rmp100@gmail.com>
2025-06-04 19:02:38 +0100

    dispaly user's data in home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require("mysql")
+
+// DB connection
+let connection = mysql.createConnection({
+  host:"127.0.0.1",
+  user:"root",
+  password:"",
+  database:"nofap"
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(req.session.login) res.render('index');
+  if(req.session.login){
+    connection.query(`select * from db_ws where username='${req.session.username}'`, (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.length > 0) {
+        res.render('index', {
+          attempt_no: results[0].attempt_no,
+          max: results[0].max,
+          start_count: results[0].start_count
+        });
+      }
+      // Prevent double render
+      return;
+    });
+    return;
+
+
+
+
+
+    res.render('index')
+  }
   else res.redirect("/logsign")
 });
 
